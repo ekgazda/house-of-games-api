e288c753bdf6cd74df5ccba458d5886373824c85
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-01 22:21:43 +0000

    create tables, insert data

---
+++ db/seeds/manage-tables.js
@@ -0,0 +1,51 @@
+const db = require('../connection')
+
+exports.dropTables = () => {
+  return db
+    .query(`DROP TABLE IF EXISTS comments;`)
+    .then(() => db.query(`DROP TABLE IF EXISTS reviews;`))
+    .then(() => db.query(`DROP TABLE IF EXISTS categories;`))
+    .then(() => db.query(`DROP TABLE IF EXISTS users;`))
+}
+
+exports.createTables = () => {
+  return db
+    .query(
+      `CREATE TABLE users (
+    username VARCHAR PRIMARY KEY,
+    name VARCHAR,
+    avatar_url TEXT);`,
+    )
+    .then(() => {
+      return db.query(
+        `CREATE TABLE categories (
+      slug VARCHAR PRIMARY KEY,
+      description TEXT);`,
+      )
+    })
+    .then(() => {
+      return db.query(
+        `CREATE TABLE reviews (
+      review_id SERIAL PRIMARY KEY,
+      title VARCHAR,
+      designer VARCHAR,
+      owner VARCHAR REFERENCES users(username) NOT NULL,
+      review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      review_body VARCHAR,
+      category VARCHAR REFERENCES categories(slug) NOT NULL,
+      created_at TIMESTAMP,
+      votes INT DEFAULT 0);`,
+      )
+    })
+    .then(() => {
+      return db.query(
+        `CREATE TABLE comments (
+      comment_id SERIAL PRIMARY KEY,
+      body TEXT,
+      votes INT DEFAULT 0,
+      author VARCHAR REFERENCES users(username) NOT NULL,
+      review_id INT REFERENCES reviews(review_id) NOT NULL,
+      created_at TIMESTAMP);`,
+      )
+    })
+}
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,78 @@
-const seed = (data) => {
-  const { categoryData, commentData, reviewData, userData } = data;
-  // 1. create tables
-  // 2. insert data
-};
+const db = require('../connection')
+const format = require('pg-format')
+const { dropTables, createTables } = require('./manage-tables')
 
-module.exports = seed;
+const seed = (data) => {
+  const { categoryData, commentData, reviewData, userData } = data
+  return dropTables()
+    .then(() => createTables())
+    .then(() => {
+      const insertedUsers = format(
+        `INSERT INTO users 
+      (username, name, avatar_url)
+      VALUES
+      %L
+      RETURNING *;`,
+        userData.map((user) => {
+          return [user.username, user.name, user.avatar_url]
+        }),
+      )
+      return db.query(insertedUsers)
+    })
+    .then(() => {
+      const insertedCategories = format(
+        `INSERT INTO categories 
+      (slug, description)
+      VALUES
+      %L
+      RETURNING *;`,
+        categoryData.map((category) => {
+          return [category.slug, category.description]
+        }),
+      )
+      return db.query(insertedCategories)
+    })
+    .then(() => {
+      const insertedReviews = format(
+        `INSERT INTO reviews 
+      (title, designer, owner, review_img_url, review_body, category, created_at, votes)
+      VALUES
+      %L
+      RETURNING *;`,
+        reviewData.map((review) => {
+          return [
+            review.title,
+            review.designer,
+            review.owner,
+            review.review_img_url,
+            review.review_body,
+            review.category,
+            review.created_at,
+            review.votes,
+          ]
+        }),
+      )
+      return db.query(insertedReviews)
+    })
+    .then(() => {
+      const insertedComments = format(
+        `INSERT INTO comments 
+      (body, votes, author, review_id, created_at)
+      VALUES
+      %L
+      RETURNING *;`,
+        commentData.map((comment) => {
+          return [
+            comment.body,
+            comment.votes,
+            comment.author,
+            comment.review_id,
+            comment.created_at,
+          ]
+        }),
+      )
+      return db.query(insertedComments)
+    })
+}
+
+module.exports = seed
