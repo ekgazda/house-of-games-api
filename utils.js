acba6172d51a4e0af8f46836e951fd8953b0f000
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-04 16:19:50 +0000

    remove utils, not used

--- utils.js
+++
@@ -1,6 +0,0 @@
-exports.convertStrValueToNum = (obj, key) => {
-  let convertedObj = { ...obj }
-  convertedObj[key] = parseInt(convertedObj[key])
-  console.log(convertedObj)
-  return convertedObj
-}
