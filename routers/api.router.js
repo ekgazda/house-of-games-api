4dcbc2021130b688f656b06cffd877417e0951f4
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-04 10:07:30 +0000

    add msg to api

--- routers/api.router.js
+++ routers/api.router.js
@@ -2,6 +2,10 @@ const apiRouter = require('express').Router()
 const categoriesRouter = require('./categories.router.js')
 const reviewsRouter = require('./reviews.router.js')
 
+apiRouter.get('/', (req, res) => {
+  res.status(200).send({ msg: 'connected to the games server' })
+})
+
 apiRouter.use('/categories', categoriesRouter)
 apiRouter.use('/reviews', reviewsRouter)
 
