e67db461d8a2f2126288e82399a0cd8ee43c9cc7
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-04 16:48:26 +0000

    add comments router

--- routers/api.router.js
+++ routers/api.router.js
@@ -1,6 +1,7 @@
 const apiRouter = require('express').Router()
-const categoriesRouter = require('./categories.router.js')
-const reviewsRouter = require('./reviews.router.js')
+const categoriesRouter = require('./categories.router')
+const reviewsRouter = require('./reviews.router')
+const commentsRouter = require('./comments.router')
 
 apiRouter.get('/', (req, res) => {
   res.status(200).send({ msg: 'connected to the games server' })
@@ -8,5 +9,6 @@ apiRouter.get('/', (req, res) => {
 
 apiRouter.use('/categories', categoriesRouter)
 apiRouter.use('/reviews', reviewsRouter)
+apiRouter.use('/comments', commentsRouter)
 
 module.exports = apiRouter
