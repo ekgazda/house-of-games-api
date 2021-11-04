f8cf077227d5dd7b92f2217f5f17f005e45bdbb7
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-04 10:06:36 +0000

    update listen port

--- listen.js
+++ listen.js
@@ -1,5 +1,7 @@
 const app = require('./app')
 
-app.listen(8888, () => {
-  console.log(`Server is listening on port 8888...`)
+const { port = 8888 } = process.env
+
+app.listen(port, () => {
+  console.log(`listening for games on ${port}...`)
 })
