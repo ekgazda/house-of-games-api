5bef70db34ecf58926c1e557b94848e7c7837079
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-05 11:45:32 +0000

    add on delete cascade and more not null

--- db/seeds/manage-tables.js
+++ db/seeds/manage-tables.js
@@ -13,27 +13,27 @@ exports.createTables = () => {
     .query(
       `CREATE TABLE users (
         username VARCHAR PRIMARY KEY,
-        name VARCHAR,
-        avatar_url TEXT);`,
+        name VARCHAR NOT NULL,
+        avatar_url TEXT NOT NULL);`,
     )
     .then(() => {
       return db.query(
         `CREATE TABLE categories (
           slug VARCHAR PRIMARY KEY,
-          description TEXT);`,
+          description TEXT NOT NULL);`,
       )
     })
     .then(() => {
       return db.query(
         `CREATE TABLE reviews (
           review_id SERIAL PRIMARY KEY,
-          title VARCHAR,
-          designer VARCHAR,
-          owner VARCHAR REFERENCES users(username),
+          title VARCHAR NOT NULL,
+          designer VARCHAR NOT NULL,
+          owner VARCHAR REFERENCES users(username) NOT NULL,
           review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
-          review_body VARCHAR,
+          review_body VARCHAR NOT NULL,
           category VARCHAR REFERENCES categories(slug) NOT NULL,
-          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+          created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
           votes INT DEFAULT 0);`,
       )
     })
@@ -41,11 +41,11 @@ exports.createTables = () => {
       return db.query(
         `CREATE TABLE comments (
           comment_id SERIAL PRIMARY KEY,
-          body TEXT,
+          body TEXT NOT NULL,
           votes INT DEFAULT 0,
           author VARCHAR REFERENCES users(username) NOT NULL,
-          review_id INT REFERENCES reviews(review_id) NOT NULL,
-          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP);`,
+          review_id INT REFERENCES reviews(review_id) ON DELETE CASCADE,
+          created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP);`,
       )
     })
 }
