5ac01e7c4f39f8c5670c822aa527295b3b08ce76
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-05 16:56:17 +0000

    update errors

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -6,13 +6,15 @@ exports.handleCustomErrors = (err, req, res, next) => {
 }
 
 exports.handlePsqlErrors = (err, req, res, next) => {
-  if (err.code === '22P02' || err.code === '23503') {
+  if (err.code === '22P02') {
     res.status(400).send({ msg: 'invalid input' })
   }
   if (err.code === '23502') {
     res.status(400).send({ msg: 'input missing' })
   }
-
+  if (err.code === '23503') {
+    res.status(404).send({ msg: 'does not exist'})
+  }
   next(err)
 }
 
